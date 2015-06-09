e08f9371b90e0df2666f509531a1bab553e42c02
James Davenport <jradavenport@gmail.com>
2015-06-09 11:56:26 -0700

    reorg less stupidly

--- appaloosa.py
+++ appaloosa.py
@@ -11,34 +11,41 @@ from detrend import polysmooth
 
 
 
-# this holds the keys to the db... don't put on github!
-auth = np.loadtxt('auth.txt',dtype='string')
+def _getLC(objectid):
 
-# connect to the db
-db = MySQLdb.connect(passwd=auth[2], db="Kepler",
-                     user=auth[1], host=auth[0])
+    # this holds the keys to the db... don't put on github!
+    auth = np.loadtxt('auth.txt',dtype='string')
+
+    # connect to the db
+    db = MySQLdb.connect(passwd=auth[2], db="Kepler",
+                         user=auth[1], host=auth[0])
+
+    query = 'SELECT QUARTER, TIME, SAP_FLUX, SAP_FLUX_ERR, SAP_QUALITY, LCFLAG, QUARTER ' \
+            'FROM Kepler.source WHERE KEPLERID='+objectid+';'
+
+    # make a cursor to the db
+    cur = db.cursor()
+    cur.execute(query)
+
+    # get all the data
+    rows = cur.fetchall()
+
+    # convert to numpy data array
+    data = np.asarray(rows)
+
+    # close the cursor to the db
+    cur.close()
+
+    return data
+
 
 
 # read the objectID from the CONDOR job...
 # objectid = sys.argv[1]
 objectid = '9726699'  # test LC
 
-query = 'SELECT QUARTER, TIME, SAP_FLUX, SAP_FLUX_ERR, SAP_QUALITY, LCFLAG ' \
-        'FROM Kepler.source WHERE KEPLERID='+objectid+';'
-
-# make a cursor to the db
-cur = db.cursor()
-cur.execute(query)
-
-# get all the data
-rows = cur.fetchall()
-
-# convert to numpy data array
-data = np.asarray(rows)
-
-# close the cursor to the db
-cur.close()
-
+# get the data from the MYSQL db
+data = _getLC(objectid)
 
 # now on to the smoothing, flare finding, flare fitting, and results!
 smo = polysmooth(data[0,:], data[1,:], data[2,:])
