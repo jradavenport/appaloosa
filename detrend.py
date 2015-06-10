64edf6c28ebc00facdf9f18ab26daa0f145d97d1
James Davenport <jradavenport@gmail.com>
2015-06-10 16:32:45 -0700

    fix order and tuple index bug

--- detrend.py
+++ detrend.py
@@ -16,7 +16,7 @@ def rolling_poly(time, flux, error, order=6, window=1.0):
 
     for i in range(0,len(w1[0])):
         x = (time[w1] >= time[w1][i] - window / 2.0) * \
-            (time[w1] <= time[w1[i]] + window / 2.0)
+            (time[w1] <= time[w1][i] + window / 2.0)
 
         fit = np.polyfit(time[w1][x], flux[w1][x], order,
                          w = (1. / error[w1][x]) )
@@ -26,7 +26,7 @@ def rolling_poly(time, flux, error, order=6, window=1.0):
     return smo
 
 
-def QtrFlat(time, flux, qtr):
+def QtrFlat(time, flux, qtr, order=3):
     '''
     step thru each unique qtr
     fit 2nd order poly to smoothed version of qtr
@@ -53,7 +53,7 @@ def QtrFlat(time, flux, qtr):
 
         indx = np.isfinite(flux_sm) # get rid of NaN's put in by rolling_median.
 
-        fit = np.polyfit(time[x][indx], flux_sm[indx], 3)
+        fit = np.polyfit(time[x][indx], flux_sm[indx], order)
 
         flux_flat[x] = flux[x] - np.polyval(fit, time[x]) + tot_med
 
