fb86103f49e5f71f6a472d0ff4f0906b89c9da18
James Davenport <jradavenport@gmail.com>
2018-03-09 09:19:32 -0500

    add upsampling

--- appaloosa/aflare.py
+++ appaloosa/aflare.py
@@ -7,6 +7,7 @@ Algorithm originally from here: https://github.com/jradavenport/FBEYE
 '''
 
 import numpy as np
+from scipy.stats import binned_statistic
 
 def aflare(t, p):
     """
@@ -56,7 +57,7 @@ def aflare(t, p):
     return flare
 
 
-def aflare1(t, tpeak, fwhm, ampl):
+def aflare1(t, tpeak, fwhm, ampl, upsample=False, uptime=10):
     '''
     The Analytic Flare Model evaluated for a single-peak (classical).
     Reference Davenport et al. (2014) http://arxiv.org/abs/1411.3723
@@ -76,6 +77,10 @@ def aflare1(t, tpeak, fwhm, ampl):
         The "Full Width at Half Maximum", timescale of the flare
     ampl : float
         The amplitude of the flare
+    upsample : bool
+        If True up-sample the model flare to ensure more precise energies.
+    uptime : float
+        How many times to up-sample the data (Default is 10)
 
     Returns
     -------
@@ -85,15 +90,43 @@ def aflare1(t, tpeak, fwhm, ampl):
     _fr = [1.00000, 1.94053, -0.175084, -2.24588, -1.12498]
     _fd = [0.689008, -1.60053, 0.302963, -0.278318]
 
-    flare = np.piecewise(t, [(t<= tpeak) * (t-tpeak)/fwhm > -1.,
-                                (t > tpeak)],
-                            [lambda x: (_fr[0]+                       # 0th order
-                                        _fr[1]*((x-tpeak)/fwhm)+      # 1st order
-                                        _fr[2]*((x-tpeak)/fwhm)**2.+  # 2nd order
-                                        _fr[3]*((x-tpeak)/fwhm)**3.+  # 3rd order
-                                        _fr[4]*((x-tpeak)/fwhm)**4. ),# 4th order
-                             lambda x: (_fd[0]*np.exp( ((x-tpeak)/fwhm)*_fd[1] ) +
-                                        _fd[2]*np.exp( ((x-tpeak)/fwhm)*_fd[3] ))]
-                            ) * np.abs(ampl) # amplitude
+    if upsample:
+        dt = np.nanmedian(np.diff(t))
+        timeup = np.linspace(min(t)-dt, max(t)+dt, t.size * uptime)
+
+        flareup = np.piecewise(timeup, [(timeup<= tpeak) * (timeup-tpeak)/fwhm > -1.,
+                                        (timeup > tpeak)],
+                                    [lambda x: (_fr[0]+                       # 0th order
+                                                _fr[1]*((x-tpeak)/fwhm)+      # 1st order
+                                                _fr[2]*((x-tpeak)/fwhm)**2.+  # 2nd order
+                                                _fr[3]*((x-tpeak)/fwhm)**3.+  # 3rd order
+                                                _fr[4]*((x-tpeak)/fwhm)**4. ),# 4th order
+                                     lambda x: (_fd[0]*np.exp( ((x-tpeak)/fwhm)*_fd[1] ) +
+                                                _fd[2]*np.exp( ((x-tpeak)/fwhm)*_fd[3] ))]
+                                    ) * np.abs(ampl) # amplitude
+
+        # and now downsample back to the original time...
+        ## this way might be better, but makes assumption of uniform time bins
+        # flare = np.nanmean(flareup.reshape(-1, uptime), axis=1)
+
+        ## This way does linear interp. back to any input time grid
+        # flare = np.interp(t, timeup, flareup)
+
+        ## this was uses "binned statistic"
+        downbins = np.concatenate((t-dt/2.,[max(t)+dt/2.]))
+        flare,_,_ = binned_statistic(timeup, flareup, statistic='mean',
+                                 bins=downbins)
+
+    else:
+        flare = np.piecewise(t, [(t<= tpeak) * (t-tpeak)/fwhm > -1.,
+                                 (t > tpeak)],
+                                [lambda x: (_fr[0]+                       # 0th order
+                                            _fr[1]*((x-tpeak)/fwhm)+      # 1st order
+                                            _fr[2]*((x-tpeak)/fwhm)**2.+  # 2nd order
+                                            _fr[3]*((x-tpeak)/fwhm)**3.+  # 3rd order
+                                            _fr[4]*((x-tpeak)/fwhm)**4. ),# 4th order
+                                 lambda x: (_fd[0]*np.exp( ((x-tpeak)/fwhm)*_fd[1] ) +
+                                            _fd[2]*np.exp( ((x-tpeak)/fwhm)*_fd[3] ))]
+                                ) * np.abs(ampl) # amplitude
 
     return flare
